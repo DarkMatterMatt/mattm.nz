993da8055583e5631e053c382ab9721b1e19b620
DarkMatterMatt <mattmoran2000@gmail.com>
2021-01-22 23:50:01 +1300

    Improve index page on mobile devices when address bar is shown

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,37 @@
 <template>
-  <VueScrollSnap :fullscreen="true">
-    <div class="item">
-      <div class="m-container">
-        <div class="m-spacer" style="max-height: 20vw" />
-        <div class="m-title m-small-caps text-h1 font-weight-regular mb-6 text-center">
-          Matt Moran
-        </div>
-        <div class="m-subtitle-container">
-          <v-icon v-ripple class="m-subtitle-icon">
-            mdi-github
-          </v-icon>
-          <v-icon v-ripple class="m-subtitle-icon mx-4">
-            mdi-pencil-box-multiple
-          </v-icon>
-          <v-icon v-ripple class="m-subtitle-icon">
-            mdi-linkedin
-          </v-icon>
-        </div>
-        <div class="m-spacer" />
-        <v-icon class="text-h3">
-          mdi-menu-down
+  <div class="m-container">
+    <div class="m-page m-flex-column">
+      <div class="m-flex-spacer" style="max-height: 20vw" />
+      <div class="m-small-caps text-h1 font-weight-regular mb-6 text-center">
+        Matt Moran
+      </div>
+      <div class="m-subtitle-container">
+        <v-icon v-ripple class="m-clickable text-h3">
+          mdi-github
+        </v-icon>
+        <v-icon v-ripple class="m-clickable text-h3 mx-4">
+          mdi-pencil-box-multiple
+        </v-icon>
+        <v-icon v-ripple class="m-clickable text-h3">
+          mdi-linkedin
         </v-icon>
       </div>
+      <div class="m-flex-spacer" />
     </div>
-    <div id="two" class="item">
-      <div class="m-container">
-        <v-icon class="text-h3">
-          mdi-menu-up
-        </v-icon>
-        <div class="m-spacer" style="max-height: 20vw" />
-        <div class="m-title m-small-caps text-h2 font-weight-regular mb-6 text-center">
-          About Me
-        </div>
-        <div />
-        <div class="m-spacer" />
+    <div class="m-between-pages m-flex-column text-center">
+      <v-icon class="text-h3 m-clickable">
+        mdi-menu-down
+      </v-icon>
+    </div>
+    <div class="m-page m-flex-column">
+      <div class="m-flex-spacer" style="max-height: 20vw" />
+      <div class="m-small-caps text-h2 font-weight-regular mb-6 text-center">
+        About Me
       </div>
+      <div />
+      <div class="m-flex-spacer" />
     </div>
-  </VueScrollSnap>
+  </div>
 </template>
 
 <script>
@@ -49,32 +44,56 @@ export default {
 </script>
 
 <style scoped>
+::-webkit-scrollbar {
+  display: none;
+}
+
 .m-small-caps {
   font-variant: small-caps;
 }
 
-.m-container {
+.m-clickable {
+  cursor: pointer;
+}
+
+.m-clickable:hover {
+  opacity: 0.7;
+}
+
+.m-flex-column {
   display: flex;
   flex-direction: column;
+}
+
+.m-flex-spacer {
+  flex: 1 1 0;
+}
+
+.m-container {
+  position: fixed;
+  overflow-y: scroll;
   height: 100%;
+  width: 100%;
+  scroll-behavior: smooth;
+  scroll-snap-type: y mandatory;
+  -webkit-overflow-scrolling: touch;
+}
+
+.m-page {
+  height: 90%;
+  width: 100%;
+  scroll-snap-align: start;
+}
+
+.m-between-pages {
+  justify-content: center;
+  height: 10%;
+  width: 100%;
 }
 
 .m-subtitle-container {
   display: flex;
-  align-items: center;
   justify-content: center;
-}
-
-.m-subtitle-icon {
-  font-size: 48px;
-  cursor: pointer;
-}
-
-.m-subtitle-icon:hover {
-  opacity: 0.7;
-}
-
-.m-spacer {
-  flex: 1 1 0;
+  align-items: center;
 }
 </style>
