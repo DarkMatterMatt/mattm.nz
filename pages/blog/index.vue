e6cc207cb92b9b5d74040682cfc8c4cfb5a47522
DarkMatterMatt <mattmoran2000@gmail.com>
2021-01-25 19:34:55 +1300

    Improve blog index page generation

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,28 +1,23 @@
 <template>
-    <v-list three-line>
-      <template v-for="(article, i) of articles">
-        <v-divider v-if="i !== 0" :key="i" />
-        <NuxtLink
-          :key="article.slug"
-          :to="{ name: 'blog-slug', params: { slug: article.slug } }"
-          class="m-no-decoration"
-        >
-          <v-list-item>
-            <v-list-item-avatar>
-              <v-img :src="article.img" />
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title v-text="article.title" />
-              <v-list-item-subtitle v-text="article.description" />
-            </v-list-item-content>
-          </v-list-item>
-        </NuxtLink>
-      </template>
-    </v-list>
+  <div>
+    <NuxtLink
+      v-for="article of articles"
+      :key="article.slug"
+      :to="{ name: 'blog-slug', params: { slug: article.slug } }"
+      class="m-no-decoration"
+    >
+      <v-card class="my-6">
+        <v-img v-if="article.img" :src="article.img" height="20vh" />
+        <v-card-title v-text="article.title" />
+        <v-card-text v-text="article.description" />
+      </v-card>
+    </NuxtLink>
+  </div>
 </template>
 
 <script>
 export default {
+    name: "Blog",
     async asyncData ({ $content, params }) {
         const articles = await $content("articles", params.slug)
             .only(["title", "description", "img", "slug", "author"])
@@ -36,6 +31,5 @@ export default {
     head: {
         title: "Blog",
     },
-    name: "Blog",
 };
 </script>
