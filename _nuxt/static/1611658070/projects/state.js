c84f612bda5aa690f80199898773df8ae213da0f
DarkMatterMatt <DarkMatterMatt@users.noreply.github.com>
2021-01-26 10:47:53 +0000

    deploy: cf4193e01dccc03380e77f7931fcdd0ff5101319

---
+++ _nuxt/static/1611658070/projects/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1611658070",layout:"empty",error:{statusCode:404,path:a,message:"This page could not be found"},state:{layout:{title:""}},serverRendered:false,routePath:a,config:{content:{dbHash:"8377375f"}}}}("\u002Fprojects"));
