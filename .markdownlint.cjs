c7ab6b4d4f8890abb9f97ab03ff5b2bc47d1f692
Matt Moran <mattmoran2000@gmail.com>
2022-11-03 16:29:06 +1300

    Rename markdownlint file

---
+++ .markdownlint.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+    "default": true,
+    "line-length": { "line_length": 100 },
+    "no-inline-html": { "allowed_elements": ["blog-after-table", "blog-before-table", "blog-caption", "blog-hr", "blog-img", "blog-undecorated-links", "br"] },
+};
--- .markdownlint.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "default": true,
-    "line-length": { "line_length": 100 },
-    "no-inline-html": { "allowed_elements": ["blog-after-table", "blog-before-table", "blog-caption", "blog-hr", "blog-img", "br"] },
-};
