1c6022c996b0b493c0b87a62ca4c9d664e56175f
DarkMatterMatt <DarkMatterMatt@users.noreply.github.com>
2022-11-05 12:17:22 +0000

    deploy: 2519ca2044680f424c744ed0b3261771ce62bd50

--- _nuxt/static/1667475636/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667475636",layout:"minimal",error:a,state:{layout:{title:""}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"edda7578"}}}}(null,"\u002F"));
